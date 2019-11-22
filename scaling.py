f8f31b888e57f5578c49bea1e01407469c486b62
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@localhost.localdomain>
2019-11-22 17:46:34 +0100

    Rigor error fixed: I've forgot an 's'

--- scaling.py
+++ scaling.py
@@ -9,7 +9,7 @@
 from matrix import Matrix
 
 def scaling(mat, m, n):
-    print(f"Scaling by factor {m} and {n}")
+    print(f"Scaling by factors {m} and {n}")
     matrix_transformation = Matrix.unit(3)
     matrix_transformation[1, 1] = m
     matrix_transformation[2, 2] = n
